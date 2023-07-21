c61c0181fac0c3e1bab58904cb48cd851f14a176
jyhyun1008 <hyun1008@kasi.re.kr>
2023-07-21 10:59:07 +0900

    으아아

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 $("#terminal").terminal(async function (command, terminal) {
     try {
         const prompt = `you are a helpful, knowledge sharing chatbot. I say: ${command}. You reply:`
-        const response = await fetch(`https://api.openai.com/v1/chat/completions`,
-                  {
-                      body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256}),
-                      method: "POST",
-                      headers: {
-                          "content-type": "application/json",
-                          "Authorization": "Bearer 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",
-                      },
-                          }
-              ).then((response) => {
-                  if (response.ok) {
+        const response = await fetch(`https://api.openai.com/v1/completions`,
+                    {
+                        body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256}),
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json",
+                            Authorization: "Bearer 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",
+                        },
+                            }
+                ).then((response) => {
                     console.log(response)
-                      response.json().then((json) => {
-                          terminal.echo(json.choices[0].text.trim());
-                      });
-                  }
-              });
+                    if (response.ok) {
+                        response.json().then((json) => {
+                            terminal.echo(json.choices[0].text.trim());
+                        });
+                    }
+                });
         console.log("Completed!");
     } catch (err) { console.error(`Error: ${err}`) }
 },
