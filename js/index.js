1ee193e5ca4e6503e0abc1a71a9a0ffc365455be
jyhyun1008 <hyun1008@kasi.re.kr>
2023-07-21 13:34:27 +0900

    제발돼야되이번엔제발돼양돼

--- js/index.js
+++ js/index.js
@@ -79,68 +79,76 @@ if (accessToken && appSecret) {
                         if (MentionRes[j].type == 'mention') {
                             var noteText = MentionRes[j].note.text
                             var noteId = MentionRes[j].note.id
-                            var prompt = `you are a helpful, knowledge sharing chatbot. I say: ${noteText}. You reply:`
-                            var sendChatUrl = 'https://api.openai.com/v1/completions'
-                            var sendChatParam = {
-                                body: JSON.stringify({
-                                    "model": "text-davinci-003", 
-                                    "prompt": prompt, 
-                                    "temperature": 0.86, 
-                                    "max_tokens": 256}),
-                                method: "POST",
-                                headers: {
-                                    "content-type": "application/json",
-                                    Authorization: "Bearer " + authCode,
-                                }
-                            }
-                            fetch(sendChatUrl, sendChatParam)
-                            .then((chatData) => {return chatData.json()})
-                            .then((chatRes) => {
-                                console.log(chatRes)
-                                if (chatRes.choices) {
-                                    var response = chatRes.choices[0].text.trim()
-                                    var replyUrl = 'https://'+host+'/api/notes/create'
-                                    var replyParam = {
-                                        method: 'POST',
-                                        headers: {
-                                            'content-type': 'application/json',
-                                        },
-                                        body: JSON.stringify({
-                                            i: i,
-                                            replyId: noteId,
-                                            text: response
-                                        }),
-                                        credentials: 'omit'
+                            if (MentionRes[j].note.repliesCount < 1) {
+
+                                var haveReply = MentionRes[j].note.reply
+                                var prompt = `you are a helpful, knowledge sharing chatbot. I say: ${noteText}. You reply:`
+                                var sendChatUrl = 'https://api.openai.com/v1/completions'
+                                var sendChatParam = {
+                                    body: JSON.stringify({
+                                        "model": "text-davinci-003", 
+                                        "prompt": prompt, 
+                                        "temperature": 0.86, 
+                                        "max_tokens": 256}),
+                                    method: "POST",
+                                    headers: {
+                                        "content-type": "application/json",
+                                        Authorization: "Bearer " + authCode,
                                     }
-                                    fetch(replyUrl, replyParam)
-                                    .then((replyData) => {return replyData.json()})
-                                    .then((replyRes) => {
-                                        if (j == MentionRes.length - 1) {
-                                            var markReadUrl = 'https://'+host+'/api/notifications/mark-all-as-read'
-                                            var markReadParam = {
-                                                method: 'POST',
-                                                headers: {
-                                                    'content-type': 'application/json',
-                                                },
-                                                body: JSON.stringify({
-                                                    i: i,
-                                                }),
-                                                credentials: 'omit'
-                                            }
-                                            fetch(markReadUrl, markReadParam)
-                                            .then((readData) => {return readData.json()})
-                                            .then((readRes) => {
-                                                setTimeout(() => {
-                                                    location.href = 'https://yeojibur.in/pichan/'
-                                                }, 10000);
-                                            })
-                                            .catch((error) => console.log(error));
-                                        }
-                                    })
-                                    .catch((error) => console.log(error));
                                 }
-                            })
-                            .catch((error) => console.log(error));
+                                fetch(sendChatUrl, sendChatParam)
+                                .then((chatData) => {return chatData.json()})
+                                .then((chatRes) => {
+                                    console.log(chatRes)
+                                    if (chatRes.choices) {
+                                        var response = chatRes.choices[0].text.trim()
+                                        var replyUrl = 'https://'+host+'/api/notes/create'
+                                        var replyParam = {
+                                            method: 'POST',
+                                            headers: {
+                                                'content-type': 'application/json',
+                                            },
+                                            body: JSON.stringify({
+                                                i: i,
+                                                replyId: noteId,
+                                                text: response
+                                            }),
+                                            credentials: 'omit'
+                                        }
+                                        fetch(replyUrl, replyParam)
+                                        .then((replyData) => {return replyData.json()})
+                                        .then((replyRes) => {
+                                            if (j == MentionRes.length - 1) {
+                                                var markReadUrl = 'https://'+host+'/api/notifications/mark-all-as-read'
+                                                var markReadParam = {
+                                                    method: 'POST',
+                                                    headers: {
+                                                        'content-type': 'application/json',
+                                                    },
+                                                    body: JSON.stringify({
+                                                        i: i,
+                                                    }),
+                                                    credentials: 'omit'
+                                                }
+                                                fetch(markReadUrl, markReadParam)
+                                                .then((readData) => {return readData.json()})
+                                                .then((readRes) => {
+                                                    setTimeout(() => {
+                                                        location.href = 'https://yeojibur.in/pichan/'
+                                                    }, 10000);
+                                                })
+                                                .catch((error) => console.log(error));
+                                            }
+                                        })
+                                        .catch((error) => console.log(error));
+                                    }
+                                })
+                                .catch((error) => console.log(error));
+                            } else if (j == MentionRes.length - 1) {
+                                setTimeout(() => {
+                                    location.href = 'https://yeojibur.in/pichan/'
+                                }, 10000);
+                            }
                         }
                     }
                 })
